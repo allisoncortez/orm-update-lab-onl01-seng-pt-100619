ae420f60eb348d0f36f61836a3b9c9bb14fa14f2
allison cortez <allizetorc@gmail.com>
2019-12-21 05:39:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,10 +45,10 @@ def self.create(name, grade)
 end
 
 def self.new_from_db(row)
-  self.new(id,name,grade) 
   id = row[0]
   name = row[1]
   grade = row[2]
+  self.new(id,name,grade)
 end
 
 def update
